471538326c607a601ae7e21e9719fe013f9b40ed
DESKTOP-2CGHMPC\WLZX <leeskiikseel@outlook.com>
2024-02-29 20:00:33 +0800

    修改分页组件显示逻辑

--- src/app/employees/employees/EmpPagination.tsx
+++ src/app/employees/employees/EmpPagination.tsx
@@ -7,14 +7,12 @@ export function EmpPagination() {
   const router = useRouter();
   const searchParams = useSearchParams();
   let currentPage = Number(searchParams.get("page") || "1");
-  const [activePage, setPage] = useState(1);
 
   //注意这里的分页逻辑，必须使用newPage，如果使用activePage，React使用的还是上次的旧值，还未得到更新
   return (
     <Pagination
-      value={currentPage}
+      defaultValue={currentPage}
       onChange={(newPage) => {
-        setPage(newPage);
         router.push(`/employees/employees?page=${newPage}`);
       }}
       total={10}
